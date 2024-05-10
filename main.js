04d52993debeb5269251cf2a349a77d7ac4c7352
ana-chiqueira <114490411+ana-chiqueira@users.noreply.github.com>
2024-05-10 08:25:06 -0300

    10/05

--- main.js
+++ main.js
@@ -1,37 +1,23 @@
-const botoes = document.querySelectorAll(".botao");
-const textos= document.querySelectorAll(".aba-conteudo");
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
-for(let i=0;i <botors.lenght;i++){
-    botoes[i].onclick = function(){
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-        
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+      tamanhoSenha = tamanhoSenha-1;   
+    }  
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < 20){
+     tamanhoSenha = tamanhoSenha+1;    
     }
+    numeroSenha.textContent = tamanhoSenha;
 }
 
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivol1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivol2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivol3 = new Date("2023-12-05T00:00:00");
-const tempoObjetivol4 = new Date("2023-102-05T00:00:00");
-
-contadores[0].textContent = calculeTempo(tempoObjetivol);
-
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
-
-    segundos %= 60;
-    minutos %= 60;
-
-    return dias + " dias " + horas + " horas "  minutos  + " minutos " + segundos + " segundos ";
-}
+console.log(botoes);
