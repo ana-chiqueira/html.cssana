bb219e8ef74fbd851ad03607016b8d01b721336a
ana-chiqueira <114490411+ana-chiqueira@users.noreply.github.com>
2024-06-07 08:30:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Aternativa 2"
+        ]
+    },
+];
